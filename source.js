1475badae9794660dcbc67fa11de89abb1866cda
Rahul Wankhede <rahulwankhedemayhem@gmail.com>
2022-12-09 20:23:34 +0530

    add function to play a round of game 5 times

--- source.js
+++ source.js
@@ -19,6 +19,10 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    for(let i = 0; i < 5; i++){
+        console.log(playRound("rock", getComputerChoice()));
+    }
+}
+
+game();

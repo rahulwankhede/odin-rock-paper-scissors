7b0e77e7b2cf3e1914fc4c4dc435a1e3eee8d725
Rahul Wankhede <rahulwankhedemayhem@gmail.com>
2022-12-09 20:12:19 +0530

    add function to play one round of game

--- source.js
+++ source.js
@@ -3,4 +3,22 @@ function getComputerChoice(){
     return randomNum === 0 ? 'Rock' : randomNum == 1 ? 'Paper' : 'Scissors';
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    let myDict = {'Rock': 1, 'Paper': 2, 'Scissors': 3};
+
+    let playerSelectionActual = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let winLoseDraw = (myDict[playerSelectionActual] - myDict[computerSelection] + 3) % 3;
+    if (winLoseDraw == 0){
+        return `It is a tie! You both selected ${computerSelection}`;
+    }
+    else if (winLoseDraw == 1){
+        return `You win! ${playerSelectionActual} beats ${computerSelection}`;
+    }
+    else{
+        return `You lose! ${computerSelection} beats ${playerSelectionActual}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));

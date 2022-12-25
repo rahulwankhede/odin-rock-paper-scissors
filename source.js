8d1c4f3c2964128c6075bc755691ebd19f7a5166
Rahul Wankhede <rahulwankhede@github.com>
2022-12-25 23:26:18 +0530

    remove Ties count display and cleanup

--- source.js
+++ source.js
@@ -9,39 +9,13 @@ function playRound(playerSelection, computerSelection){
     return drawWinLose = (myDict[playerSelectionActual] - myDict[computerSelection] + 3) % 3;
 }
 
-function game(){
-    let winDict = {"player": 0, "computer": 0, "tie": 0};
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("Make your choice. Type rock, paper, or scissors in this box");
-        let computerSelection = getComputerChoice();
-        let drawWinLose = playRound(playerSelection, computerSelection);
-        if (drawWinLose == 0){
-            console.log(`It is a tie! You both selected ${computerSelection}`);
-            winDict["tie"] += 1;
-        }
-        else if (drawWinLose == 1){
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-            winDict["player"] += 1;
-        }
-        else{
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-            winDict["computer"] += 1
-        }
-    }
-    console.log(
-    winDict["player"] > winDict["computer"] ? "You win!" :
-    winDict["player"] < winDict["computer"] ? "You lose!" :
-    "It's a tie!");
-    console.log(`You: ${winDict["player"]}; Computer: ${winDict["computer"]}; Ties: ${winDict["tie"]}`);
-}
-
 let winDict = {"player": 0, "computer": 0, "tie": 0};
 
 const resultDiv = document.querySelector(".results");
 const scoreDiv = document.querySelector(".score");
 const finalDiv = document.querySelector(".final");
 
-function doSomething(e){
+function playGame(e){
 	const playerSelection = e.target.textContent;
     const computerSelection = getComputerChoice();
 
@@ -59,7 +33,7 @@ function doSomething(e){
 		winDict["computer"] += 1;
         resultDiv.innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
 	}
-	scoreDiv.innerText = `You: ${winDict["player"]}	Computer: ${winDict["computer"]}	Ties: ${winDict["tie"]}`
+	scoreDiv.innerText = `You: ${winDict["player"]} Computer: ${winDict["computer"]}`
 
 	if (winDict["player"] == 5 || winDict["computer"] == 5){
 		finalDiv.innerText = `Game over. You ${winDict["player"] == 5 ? "win!" : "lose!"}\n Refresh page to play again`;
@@ -70,5 +44,5 @@ function doSomething(e){
 const buttons = document.querySelectorAll(".choice");
 
 for (button of buttons){
-	button.addEventListener('click', doSomething);
+	button.addEventListener('click', playGame);
 }
